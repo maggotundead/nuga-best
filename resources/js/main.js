d84fe9df42f5fa539fb5228651c1b3431f826854
Roman Salko <maggotundead@yandex.com>
2022-08-30 18:37:52 +0300

    desktop update

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,7 +10,7 @@ const scrollbarSwiper = new Swiper('.js-scrollbar-swiper', {
     mousewheel: {
         forceToAxis: true,
     },
-    slideToClickedSlide: true,
+    // slideToClickedSlide: true,
     keyboard: {
         enabled: true,
         onlyInViewport: true
@@ -21,3 +21,26 @@ const scrollbarSwiper = new Swiper('.js-scrollbar-swiper', {
     },
 });
 
+const tabsGroup = document.querySelectorAll('.tabs');
+if (tabsGroup) {
+    tabsGroup.forEach(tabsGroup => {
+        let tabSelectors = tabsGroup.querySelector('.tab-selectors');
+        let tabButton = tabSelectors.querySelectorAll('.tab-selector');
+        let tabs = tabsGroup.querySelectorAll('.tab');
+
+        tabButton.forEach( ( btn, i ) => {
+            btn.setAttribute('index', i);
+            btn.onclick = function() {
+                for (let i = 0; i < tabButton.length; i++) {
+                    tabButton[i].classList.remove('active');
+                }
+                this.className = 'tab-selector active';
+                let index = this.getAttribute('index');
+                for (let i = 0; i < tabs.length; i++) {
+                    tabs[i].classList.remove('active');
+                }
+                tabs[i].classList.add('active');
+            }
+        });
+    });
+}
