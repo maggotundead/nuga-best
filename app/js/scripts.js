b0bbf6e6d0f440227871d295d5a621138aaad606
Roman Salko <maggotundead@yandex.com>
2022-09-01 02:18:54 +0300

    site beta

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -72,3 +72,66 @@ const topProductsSwiperMobile = new Swiper('.js-top-products-swiper-mobile', {
         draggable: true,
     },
 });
+
+// modals
+const modalCallbackOpener = document.querySelectorAll('[js-modal-callback-btn]');
+const modalOverlay = document.querySelectorAll('.modal-overlay');
+const modalClose = document.querySelectorAll('.modal-close');
+
+const modalCallback = document.querySelector('.modal-callback');
+
+modalCallbackOpener.forEach(btn => {
+    btn.addEventListener('click', () => {
+        modalCallback.classList.add('active');
+    });
+});
+modalOverlay.forEach( bg => {
+    bg.addEventListener('click', () => {
+        bg.closest('.modal').classList.remove('active');
+    });
+});
+modalClose.forEach( btn => {
+    btn.addEventListener('click', () => {
+        btn.closest('.modal').classList.remove('active');
+    });
+});
+
+// end modals
+
+// header mobile
+const header = document.querySelector('header');
+
+header.querySelector('.burger-btn').addEventListener('click', () => {
+    document.querySelector('body').classList.toggle('mobile-menu-open');
+});
+
+const mobileMenuDropdownToggler = header.querySelectorAll('.js-mobile-dropdown-toggler');
+mobileMenuDropdownToggler.forEach(btn => {
+    btn.addEventListener('click', e => {
+        e.preventDefault();
+        btn.classList.toggle('active');
+    });
+});
+
+const mobileSearchOpener = header.querySelector('.js-mobile-search-opener');
+const mobileSearchCloser = header.querySelector('.js-mobile-search-closer');
+const mobileSearch = header.querySelector('.search');
+mobileSearchOpener.addEventListener('click', () => {
+    mobileSearch.classList.add('active');
+});
+mobileSearchCloser.addEventListener('click', (e) => {
+    e.preventDefault();
+    mobileSearch.classList.remove('active');
+});
+// end header mobile
+
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         document.body.classList.contains('mobile-menu-open') && document.body.classList.remove('mobile-menu-open');
+
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
